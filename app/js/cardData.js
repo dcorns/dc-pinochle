0e04eb5277a398ecf9ef34170df865836f5b3e6d
Dale Corns <drcorns@hotmail.com>
2015-09-15 15:38:09 -0700

    add pinochle melding

--- app/js/cardData.js
+++ app/js/cardData.js
@@ -14,44 +14,89 @@ function Meld(hand){
   this.clubs = [];
   this.diamonds = [];
   this.hearts = [];
-  this.jacksAround = {basePoints: 4, points: 0}
+  this.jacksAround = {baseInt: 1, basePoints: 4, points: 0};
+  this.queensAround = {baseInt: 2, basePoints: 6, points: 0};
+  this.kingsAround = {baseInt: 3, basePoints: 8, points: 0};
+  this.tensAround = {baseInt: 4, basePoints: 0, points: 0};
+  this.acesAround = {baseInt: 5, basePoints: 10, points: 0};
+  this.pinochle = {basePoints: 4, points: 0};
+  this.marriage = {basePoints: 2, points: 0};
 }
-Meld.prototype.recordArounds = function(baseInt){
+Meld.prototype.recordArounds = function(meldObj){
   var c = 0,
     spadeCount = 0,
     clubCount = 0,
     diamondCount = 0,
     heartCount = 0;
   for(c; c < this.len; c++){
-    if(this.hand[c] === baseInt || this.hand[c] === baseInt + 20 || this.hand[c] === baseInt + 40 || this.hand[c] === baseInt + 60){
+    if(this.hand[c] === meldObj.baseInt || this.hand[c] === meldObj.baseInt + 5 || this.hand[c] === meldObj.baseInt + 10 || this.hand[c] === meldObj.baseInt + 15){
       this.spades.push(this.hand[c]);
       spadeCount ++;
     }
-    else if (this.hand[c] === baseInt + 5 || this.hand[c] === baseInt + 25 || this.hand[c] === baseInt + 45 || this.hand[c] === baseInt + 65){
+    else if (this.hand[c] === meldObj.baseInt + 20 || this.hand[c] === meldObj.baseInt + 25 || this.hand[c] === meldObj.baseInt + 30 || this.hand[c] === meldObj.baseInt + 35){
       this.clubs.push(this.hand[c]);
       clubCount++;
     }
-    else if (this.hand[c] === baseInt + 10 || this.hand[c] === baseInt + 30 || this.hand[c] === baseInt + 50 || this.hand[c] === baseInt + 70){
+    else if (this.hand[c] === meldObj.baseInt + 40 || this.hand[c] === meldObj.baseInt + 45 || this.hand[c] === meldObj.baseInt + 50 || this.hand[c] === meldObj.baseInt + 55){
       this.diamonds.push(this.hand[c]);
       diamondCount++;
     }
-    else if (this.hand[c] === baseInt + 15 || this.hand[c] === baseInt + 35 || this.hand[c] === baseInt + 55 || this.hand[c] === baseInt + 75){
+    else if (this.hand[c] === meldObj.baseInt + 60 || this.hand[c] === meldObj.baseInt + 65 || this.hand[c] === meldObj.baseInt + 70 || this.hand[c] === meldObj.baseInt + 75){
       this.hearts.push(this.hand[c]);
       heartCount++;
     }
-    if(spadeCount > 0 && clubCount > 0 && diamondCount > 0 && heartCount > 0){
-      this.jacksAround.points = this.jacksAround.basePoints;
-      if(spadeCount > 1 && clubCount > 1 && diamondCount > 1 && heartCount > 1){
-        this.jacksAround.points = this.jacksAround.basePoints * 10;
-        if(spadeCount > 2 && clubCount > 2 && diamondCount > 2 && heartCount > 2){
-          this.jacksAround.points = this.jacksAround.basePoints * 10 + 4;
-          if(spadeCount === 4 === clubCount === diamondCount === heartCount){
-            this.jacksAround.points = this.jacksAround.basePoints * 20;
-          }
+  }
+  console.log(spadeCount, clubCount, diamondCount, heartCount);
+  if(spadeCount > 0 && clubCount > 0 && diamondCount > 0 && heartCount > 0){
+    meldObj.points = meldObj.basePoints;
+    if(spadeCount > 1 && clubCount > 1 && diamondCount > 1 && heartCount > 1){
+      meldObj.points = meldObj.basePoints * 10;
+      if(spadeCount > 2 && clubCount > 2 && diamondCount > 2 && heartCount > 2){
+        meldObj.points = meldObj.basePoints * 10 + meldObj.basePoints;
+        if(spadeCount > 3 && clubCount > 3 && diamondCount > 3 && heartCount > 3){
+          meldObj.points = meldObj.basePoints * 20;
         }
       }
     }
   }
 };
+Meld.prototype.calculateMeld = function(){
+  this.recordArounds(this.jacksAround);
+  this.recordArounds(this.queensAround);
+  this.recordArounds(this.kingsAround);
+  this.recordArounds(this.tensAround);
+  this.recordArounds(this.acesAround);
+  this.recordPinochle();
+};
+Meld.prototype.recordPinochle = function(){
+  var jackCount = 0, queenCount = 0, c = 0, len = this.spades.length;
+  for(c; c < len; c++){
+    if(this.spades[c] === 1 || this.spades[c] === 6 || this.spades[c] === 11 || this.spades[c] === 16){
+      jackCount++;
+    }
+  }
+  c = 0; len = this.diamonds.length;
+  for(c; c < len; c++){
+    if(this.diamonds[c] === 42 || this.diamonds[c] === 47 || this.diamonds[c] === 52 || this.diamonds[c] === 57){
+      queenCount++;
+    }
+  }
+  if(jackCount > 0 && queenCount > 0){
+    this.pinochle.points = this.pinochle.basePoints;
+    if(jackCount > 1 && queenCount > 1){
+      this.pinochle.points = this.pinochle.basePoints * 10;
+      if(jackCount > 2 && queenCount > 2){
+        this.pinochle.points = this.pinochle.basePoints * 20 + 10;
+        if(jackCount > 3 && queenCount > 3){
+          this.pinochle.points = 500;//and the game
+        }
+      }
+    }
+  }
+};
+Meld.prototype.recordMarriages = function(){
+  var kingS, queenS, kingC, queenC, kingD, queenD, kingH, queenH;
+  var c = 0, len = this.spades.length;
 
+};
 module.exports = Meld;
--- test/api/cardData_test.js
+++ test/api/cardData_test.js
@@ -7,87 +7,153 @@
 'use strict';
 var expect = require('chai').expect;
 
-describe('meld points',function(){
+describe('meld points for arounds',function() {
+  var Meld;
+  beforeEach(function () {
+    Meld = require('../../app/js/cardData');
+  });
+  it('Meld is a constructor function', function () {
+    expect(typeof Meld).to.be.eq('function');
+  });
+  it('has 4 point base for Jacks around', function () {
+    var meld = new Meld([]);
+    expect(meld.jacksAround.basePoints).to.be.eq(4);
+  });
+  it('scores jacks around', function () {
+    var meld = new Meld([1, 21, 41, 61]);
+    meld.recordArounds(meld.jacksAround);
+    expect(meld.jacksAround.points).to.be.eq(4);
+  });
+  it('scores double jacks around', function () {
+    var meld = new Meld([1, 6, 21, 26, 41, 46, 61, 66]);
+    meld.recordArounds(meld.jacksAround);
+    expect(meld.jacksAround.points).to.be.eq(40);
+  });
+  it('scores triple jacks around', function () {
+    var meld = new Meld([1, 6, 11, 21, 26, 31, 41, 46, 51, 61, 66, 71]);
+    meld.recordArounds(meld.jacksAround);
+    expect(meld.jacksAround.points).to.be.eq(44);
+  });
+  it('scores quad jacks around', function () {
+    var meld = new Meld([1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51, 56, 61, 66, 71, 76]);
+    meld.recordArounds(meld.jacksAround);
+    expect(meld.jacksAround.points).to.be.eq(80);
+  });
+  it('has 6 point base for Queens around', function () {
+    var meld = new Meld([]);
+    expect(meld.queensAround.basePoints).to.be.eq(6);
+  });
+  it('scores Queens around', function () {
+    var meld = new Meld([2, 22, 42, 62]);
+    meld.recordArounds(meld.queensAround);
+    expect(meld.queensAround.points).to.be.eq(6);
+  });
+  it('scores double Queens around', function () {
+    var meld = new Meld([2, 7, 22, 27, 42, 47, 62, 67]);
+    meld.recordArounds(meld.queensAround);
+    expect(meld.queensAround.points).to.be.eq(60);
+  });
+  it('scores triple Queens around', function () {
+    var meld = new Meld([2, 7, 12, 22, 27, 32, 42, 47, 52, 62, 67, 72]);
+    meld.recordArounds(meld.queensAround);
+    expect(meld.queensAround.points).to.be.eq(66);
+  });
+  it('scores quad Queens around', function () {
+    var meld = new Meld([2, 7, 12, 17, 22, 27, 32, 37, 42, 47, 52, 57, 62, 67, 72, 77]);
+    meld.recordArounds(meld.queensAround);
+    expect(meld.queensAround.points).to.be.eq(120);
+  });
+  it('has 8 point base for Kings around', function () {
+    var meld = new Meld([]);
+    expect(meld.kingsAround.basePoints).to.be.eq(8);
+  });
+  it('scores Kings around', function () {
+    var meld = new Meld([3, 23, 43, 63]);
+    meld.recordArounds(meld.kingsAround);
+    expect(meld.kingsAround.points).to.be.eq(8);
+  });
+  it('scores double Kings around', function () {
+    var meld = new Meld([3, 8, 23, 28, 43, 48, 63, 68]);
+    meld.recordArounds(meld.kingsAround);
+    expect(meld.kingsAround.points).to.be.eq(80);
+  });
+  it('scores triple Kings around', function () {
+    var meld = new Meld([3, 8, 13, 23, 28, 33, 43, 48, 53, 63, 68, 73]);
+    meld.recordArounds(meld.kingsAround);
+    expect(meld.kingsAround.points).to.be.eq(88);
+  });
+  it('scores quad Kings around', function () {
+    var meld = new Meld([3, 8, 13, 18, 23, 28, 33, 38, 43, 48, 53, 58, 63, 68, 73, 78]);
+    meld.recordArounds(meld.kingsAround);
+    expect(meld.kingsAround.points).to.be.eq(160);
+  });
+  it('has 10 point base for Aces around', function () {
+    var meld = new Meld([]);
+    expect(meld.acesAround.basePoints).to.be.eq(10);
+  });
+  it('scores Aces around', function () {
+    var meld = new Meld([5, 25, 45, 65]);
+    meld.recordArounds(meld.acesAround);
+    expect(meld.acesAround.points).to.be.eq(10);
+  });
+  it('scores double Aces around', function () {
+    var meld = new Meld([5, 10, 25, 30, 45, 50, 65, 70]);
+    meld.recordArounds(meld.acesAround);
+    expect(meld.acesAround.points).to.be.eq(100);
+  });
+  it('scores triple Aces around', function () {
+    var meld = new Meld([5, 10, 15, 25, 30, 35, 45, 50, 55, 65, 70, 75]);
+    meld.recordArounds(meld.acesAround);
+    expect(meld.acesAround.points).to.be.eq(110);
+  });
+  it('scores quad Aces around', function () {
+    var meld = new Meld([5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80]);
+    meld.recordArounds(meld.acesAround);
+    expect(meld.acesAround.points).to.be.eq(200);
+  });
+});
+describe('Other meld points', function(){
   var Meld;
   beforeEach(function(){
     Meld = require('../../app/js/cardData');
   });
-  it('Meld is a constructor function', function(){
-    expect(typeof Meld).to.be.eq('function');
-  });
-  it('has 4 point base for Jacks around', function(){
-    var meld = new Meld([]);
-    expect(meld.jacksAround.basePoints).to.be.eq(4);
-  });
-  it('scores jacks around', function(){
-    var meld = new Meld([1,21,41,61]);
-    console.dir(meld);
-    meld.recordArounds(1);
-    expect(meld.jacksAround.points).to.be.eq(4);
-  });
-  it('scores double jacks around', function(){
-    var hand = [1, 6, 21, 26, 41, 46, 61, 66],
-      meld = cardData.scoreAround(hand, 1, cardData.jacksAround.points);
-    expect(meld).to.be.eq(40);
-  });
-  it('has 6 point base for Queens around', function(){
-    expect(cardData.queensAround.points).to.be.eq(6);
-  });
-  it('scores queens around', function(){
-    var hand = [2,22,42,62],
-      meld = cardData.scoreAround(hand, 2, cardData.queensAround.points);
-    expect(meld).to.be.eq(6);
-  });
-  it('scores double queens around', function(){
-    var hand = [2, 7, 22, 27, 42, 47, 62, 67],
-      meld = cardData.scoreAround(hand, 2, cardData.queensAround.points);
-    expect(meld).to.be.eq(60);
-  });
-  it('has 8 point base for Kings around', function(){
-    expect(cardData.kingsAround.points).to.be.eq(8);
-  });
-  it('scores kings around', function(){
-    var hand = [3,23,43,63],
-      meld = cardData.scoreAround(hand, 3, cardData.kingsAround.points);
-    expect(meld).to.be.eq(8);
-  });
-  it('scores double kings around', function(){
-    var hand = [3, 8, 23, 28, 43, 48, 63, 68],
-      meld = cardData.scoreAround(hand, 3, cardData.kingsAround.points);
-    expect(meld).to.be.eq(80);
-  });
-  it('has 10 point base for Aces around', function(){
-    expect(cardData.acesAround.points).to.be.eq(10);
-  });
-  it('scores aces around', function(){
-    var hand = [5,25,45,65],
-      meld = cardData.scoreAround(hand, 5, cardData.acesAround.points);
-    expect(meld).to.be.eq(10);
-  });
-  it('scores double aces around', function(){
-    var hand = [5, 10, 25, 30, 45, 50, 65, 70],
-      meld = cardData.scoreAround(hand, 5, cardData.acesAround.points);
-    expect(meld).to.be.eq(100);
-  });
   it('has 4 point base for pinochle', function(){
-    expect(cardData.pinochle.points).to.be.eq(4);
+    var meld = new Meld([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80]);
+    meld.calculateMeld();
+    expect(meld.pinochle.basePoints).to.be.eq(4);
   });
-  it('has 2 point base for marriage', function(){
-    expect(cardData.marriage.points).to.be.eq(2);
+  it('scores 4 points for double pinochle', function(){
+    var meld = new Meld([1,2,3,4,5,7,8,9,10,12,13,14,15,17,18,19,20,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60]);
+    meld.calculateMeld();
+    expect(meld.pinochle.points).to.be.eq(4);
   });
-  it('has 30 point base for double pinochle', function(){
-    expect(cardData.pinochle2.points).to.be.eq(30);
+  it('scores 40 points for double pinochle', function(){
+    var meld = new Meld([1,2,3,4,5,6,7,8,9,10,12,13,14,15,17,18,19,20,41,42,43,44,45,46,47,48,49,50,51,53,54,55,56,58,59,60]);
+    meld.calculateMeld();
+    expect(meld.pinochle.points).to.be.eq(40);
   });
-  it('has 90 point base for triple pinochle', function(){
-    expect(cardData.pinochle3.points).to.be.eq(90);
+  it('scores 90 points for triple pinochle', function(){
+    var meld = new Meld([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,17,18,19,20,41,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60]);
+    meld.calculateMeld();
+    expect(meld.pinochle.points).to.be.eq(90);
   });
-  it('has 500 point base for quad pinochle', function(){
-    expect(cardData.pinochle4.points).to.be.eq(500);
+  it('scores 500 points (game) for double pinochle', function(){
+    var meld = new Meld([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60]);
+    meld.calculateMeld();
+    expect(meld.pinochle.points).to.be.eq(500);
   });
-  it('has 24 point base for round robin', function(){
-    expect(cardData.roundRobin.points).to.be.eq(24);
+  it('2 point base for marriage', function(){
+    var meld = new Meld([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80]);
+    meld.calculateMeld();
+    expect(meld.marriage.basePoints).to.be.eq(2);
   });
-  it('has a 15 point base for simple run', function(){
-    expect(cardData.runn.points).to.be.eq(15);
-  });
+  //it('separates all clubs', function(){
+  //  expect(meld.clubs).to.be.eq([ 21, 26, 31, 36, 22, 27, 32, 37, 23, 28, 33, 38, 24, 29, 34, 39, 25, 30, 35, 40 ]);
+  //});
+  //it('separates all diamonds', function(){
+  //  expect(meld.diamonds).to.be.eq([ 41, 46, 51, 56, 42, 47, 52, 57, 43, 48, 53, 58, 44, 49, 54, 59, 45, 50, 55, 60 ]);
+  //});
+  //it('separates all hearts', function(){
+  //  expect(meld.hearts).to.be.eq([ 61, 66, 71, 76, 62, 67, 72, 77, 63, 68, 73, 78, 64, 64, 69, 74, 79, 65, 70, 75, 80 ]);
+  //});
 });
