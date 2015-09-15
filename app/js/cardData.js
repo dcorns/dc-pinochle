c362f395746663936767b4316e070a6310f472c6
Dale Corns <drcorns@hotmail.com>
2015-09-14 21:41:59 -0700

    some ideas no keep just in case

--- app/js/cardData.js
+++ app/js/cardData.js
@@ -2,34 +2,89 @@
  * cardData
  * Created by dcorns on 9/13/15
  * Copyright © 2015 Dale Corns
+ * Define points and scoring card structures
+ * Cards are two pinochle decks without nines. 1 - 80 represent all cards like so (jack-Ace) * 4 in spades followed by clubs, diamonds and hearts
  */
 'use strict';
-var cardData = {};
-cardData.cardKey = [];
-var c = 0,
-  r = 1;
 
-while(c < 20){
-  if(r === 6) r = 1;
-  cardData.cardKey[c] = {suite: 1, rank: r};
-  cardData.cardKey[c+20] = {suite: 2, rank: r};
-  cardData.cardKey[c+40] = {suite: 3, rank: r};
-  cardData.cardKey[c+60] = {suite: 4, rank: r};
-  c++;
-  r++;
-}
+var cardData = {
+  jacksAround: {
+    points: 4,
+    checkCard: function(meld, card){
+      var jacks = [1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51, 56, 61, 66, 71, 76];
 
-cardData.meldPoints = {
-  jacksAround: 4,
-  queensAround: 6,
-  kingsAround: 8,
-  acesAround: 10,
-  pinochle: 4,
-  pinochle2: 30,
-  pinochle3: 90,
-  pinochle4: 500,
-  marriage: 2,
-  roundRobin: 24,
-  run: 15
+    }
+  },
+  queensAround: {
+    points: 6
+  },
+  kingsAround: {
+    points: 8
+  },
+  acesAround: {
+    points: 10
+  },
+  pinochle: {
+    points: 4
+  },
+  pinochle2: {
+    points: 30
+  },
+  pinochle3: {
+    points: 90
+  },
+  pinochle4: {
+    points: 500
+  },
+  marriage: {
+    points: 2
+  },
+  roundRobin: {
+    points: 24
+  },
+  runn: {
+    points: 15
+  },
+  scoreAround: function(hand, basePosition, basePoints) {
+    var c = 0,
+      clubs = basePosition + 20,
+      diamonds = basePosition + 40,
+      hearts = basePosition + 60,
+      len = hand.length,
+      points = 0;
+    var spadeCount = 0, clubsCount = 0, diamondsCount = 0, heartsCount = 0;
+    for (c; c < len; c++) {
+      if (hand[c] === basePosition || hand[c] === basePosition + 5 || hand[c] === basePosition + 10 || hand[c] === basePosition + 15) {
+        spadeCount++;
+      }
+      else if (hand[c] === clubs || hand[c] === clubs + 5 || hand[c] === clubs + 10 || hand[c] === clubs + 15) {
+        clubsCount++;
+      }
+      else if (hand[c] === diamonds || hand[c] === diamonds + 5 || hand[c] === diamonds + 10 || hand[c] === diamonds + 15) {
+        diamondsCount++;
+      }
+      else if (hand[c] === hearts || hand[c] === hearts + 5 || hand[c] === hearts + 10 || hand[c] === hearts + 15) {
+        heartsCount++;
+      }
+    }
+    if (spadeCount > 0 && clubsCount > 0 && diamondsCount > 0 && heartsCount > 0) {
+      points = basePoints;
+      if (spadeCount > 1 && clubsCount > 1 && diamondsCount > 1 && heartsCount > 1) {
+        points = basePoints * 10;
+      }
+    }
+    return points;
+  },
+  scorePinochle: function(hand){
+
+  },
+  assignHand: function(hand){
+    var meld = {total: 0},
+      c = 0,
+      len = hand.length;
+    for(c; c < len; c++){
+
+    }
+  }
 };
 module.exports = cardData;
--- test/api/cardData_test.js
+++ test/api/cardData_test.js
@@ -6,98 +6,86 @@
  */
 'use strict';
 var expect = require('chai').expect;
-describe('Build Card Object array called cardKey', function(){
-  var cardData;
-  beforeEach(function(){
-    cardData = require('../../app/js/cardData');
-  });
-  it('returns an object with a cardKey property that is an array of 80 card objects', function(){
-    expect(Array.isArray(cardData.cardKey)).true;
-    expect(cardData.cardKey.length).to.be.eq(80);
-    expect(function(){
-      var c = 0;
-      for(c; c < 80; c++){
-        if(!(cardData.cardKey[c].hasOwnProperty('suite'))) return false;
-        if(!(cardData.cardKey[c].hasOwnProperty('rank'))) return false;
-      }
-      return true;
-    }()).true;
-  });
-});
-
-describe('Card Objects', function(){
-  var cardData;
-  beforeEach(function(){
-    cardData = require('../../app/js/cardData');
-  });
-  it('has 4 suites of 20 cards each', function(){
-    expect(function(){
-      var c = 0;
-      for(c; c < 20; c++){
-        if(!(cardData.cardKey[c].suite === 1)) return false;//spades
-        if(!(cardData.cardKey[c + 20].suite === 2)) return false;//clubs
-        if(!(cardData.cardKey[c + 40].suite === 3)) return false;//hearts
-        if(!(cardData.cardKey[c + 60].suite === 4)) return false;//diamonds
-      }
-      return true;
-    }()).true;
-  });
-  it('has rank from 1 - 5 * 4 for each suite', function(){
-    expect(function(){
-      var c = 0,
-        r = 1;
-      for(c; c < 20; c++){
-        if(!(cardData.cardKey[c].rank === r)) return false;
-        if(!(cardData.cardKey[c + 20].rank === r)) return false;
-        if(!(cardData.cardKey[c + 40].rank === r)) return false;
-        if(!(cardData.cardKey[c + 60].rank === r)) return false;
-        r++;
-        if(r === 6) r = 1;
-      }
-      return true;
-    }()).true;
-  });
-});
 
 describe('meld points',function(){
   var cardData;
   beforeEach(function(){
     cardData = require('../../app/js/cardData');
   });
-  it('has a property call meldPoints', function(){
-    expect(typeof cardData.meldPoints).to.be.eq('object');
+  it('is an object', function(){
+    expect(typeof cardData).to.be.eq('object');
   });
   it('has 4 point base for Jacks around', function(){
-    expect(cardData.meldPoints.jacksAround).to.be.eq(4);
+    expect(cardData.jacksAround.points).to.be.eq(4);
+  });
+  it('scores jacks around', function(){
+    var hand = [1,21,41,61],
+      meld = cardData.scoreAround(hand, 1, cardData.jacksAround.points);
+    expect(meld).to.be.eq(4);
+  });
+  it('scores double jacks around', function(){
+    var hand = [1, 6, 21, 26, 41, 46, 61, 66],
+      meld = cardData.scoreAround(hand, 1, cardData.jacksAround.points);
+    expect(meld).to.be.eq(40);
   });
   it('has 6 point base for Queens around', function(){
-    expect(cardData.meldPoints.queensAround).to.be.eq(6);
+    expect(cardData.queensAround.points).to.be.eq(6);
+  });
+  it('scores queens around', function(){
+    var hand = [2,22,42,62],
+      meld = cardData.scoreAround(hand, 2, cardData.queensAround.points);
+    expect(meld).to.be.eq(6);
+  });
+  it('scores double queens around', function(){
+    var hand = [2, 7, 22, 27, 42, 47, 62, 67],
+      meld = cardData.scoreAround(hand, 2, cardData.queensAround.points);
+    expect(meld).to.be.eq(60);
   });
   it('has 8 point base for Kings around', function(){
-    expect(cardData.meldPoints.kingsAround).to.be.eq(8);
+    expect(cardData.kingsAround.points).to.be.eq(8);
+  });
+  it('scores kings around', function(){
+    var hand = [3,23,43,63],
+      meld = cardData.scoreAround(hand, 3, cardData.kingsAround.points);
+    expect(meld).to.be.eq(8);
+  });
+  it('scores double kings around', function(){
+    var hand = [3, 8, 23, 28, 43, 48, 63, 68],
+      meld = cardData.scoreAround(hand, 3, cardData.kingsAround.points);
+    expect(meld).to.be.eq(80);
   });
   it('has 10 point base for Aces around', function(){
-    expect(cardData.meldPoints.acesAround).to.be.eq(10);
+    expect(cardData.acesAround.points).to.be.eq(10);
+  });
+  it('scores aces around', function(){
+    var hand = [5,25,45,65],
+      meld = cardData.scoreAround(hand, 5, cardData.acesAround.points);
+    expect(meld).to.be.eq(10);
+  });
+  it('scores double aces around', function(){
+    var hand = [5, 10, 25, 30, 45, 50, 65, 70],
+      meld = cardData.scoreAround(hand, 5, cardData.acesAround.points);
+    expect(meld).to.be.eq(100);
   });
   it('has 4 point base for pinochle', function(){
-    expect(cardData.meldPoints.pinochle).to.be.eq(4);
+    expect(cardData.pinochle.points).to.be.eq(4);
   });
   it('has 2 point base for marriage', function(){
-    expect(cardData.meldPoints.marriage).to.be.eq(2);
+    expect(cardData.marriage.points).to.be.eq(2);
   });
   it('has 30 point base for double pinochle', function(){
-    expect(cardData.meldPoints.pinochle2).to.be.eq(30);
+    expect(cardData.pinochle2.points).to.be.eq(30);
   });
   it('has 90 point base for triple pinochle', function(){
-    expect(cardData.meldPoints.pinochle3).to.be.eq(90);
+    expect(cardData.pinochle3.points).to.be.eq(90);
   });
   it('has 500 point base for quad pinochle', function(){
-    expect(cardData.meldPoints.pinochle4).to.be.eq(500);
+    expect(cardData.pinochle4.points).to.be.eq(500);
   });
   it('has 24 point base for round robin', function(){
-    expect(cardData.meldPoints.roundRobin).to.be.eq(24);
+    expect(cardData.roundRobin.points).to.be.eq(24);
   });
   it('has a 15 point base for simple run', function(){
-    expect(cardData.meldPoints.run).to.be.eq(15);
+    expect(cardData.runn.points).to.be.eq(15);
   });
 });
